0079ffaeb574cbc5b873bb1324683acf70aa8ba0
kwmorlock <kmorlockcodes@gmail.com>
2020-12-15 21:09:25 -0500

    set: App.js App

--- App.js
+++ App.js
@@ -1,1 +1,18 @@
-//setup
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          Test
+        </p>
+        <a>
+          Test
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
