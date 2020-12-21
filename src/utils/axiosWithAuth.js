3a1be66a829b24d443f19ea7b763da75b3ed5a9e
kwmorlock <kmorlockcodes@gmail.com>
2020-12-20 19:09:33 -0500

    set: axiosWithAuth.js token

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -1,1 +1,12 @@
-//setup
+import axios from "axios";
+
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem("token");
+
+  return axios.create({
+    baseURL: "https://lgbtqstories.herokuapp.com/",
+    headers: {
+      Authorization: token,
+    },
+  });
+};
