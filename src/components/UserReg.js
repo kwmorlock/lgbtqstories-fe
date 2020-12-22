62ad10d4048322c98fd0f5c7e510ee4736fff7cb
kwmorlock <kmorlockcodes@gmail.com>
2020-12-22 00:43:42 -0500

    set: UserReg.js Register

--- src/components/UserReg.js
+++ src/components/UserReg.js
@@ -1,3 +1,11 @@
 import React, { useState, useEffect } from "react";
 import * as yup from "yup";
 import { axiosWithAuth } from ".././utils/axiosWithAuth";
+
+const Register = (props) => {
+  const [userData, setUserData] = useState({
+    username: "",
+    password: "",
+    email: "",
+  });
+};
