db57fca4848deee1194ffe83f4284705971a6974
kwmorlock <kmorlockcodes@gmail.com>
2020-12-23 16:46:21 -0500

    set: App.js Register route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Register from "./components/UserReg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <p>Hello</p>
+      <Switch>
+        <Route path="/api/users/auth/register" component={Register} />
+      </Switch>
     </div>
   );
 }
