697e104d2a26d35a8d88bcef370e5ba6e34f7a96
kwmorlock <kmorlockcodes@gmail.com>
2020-12-24 00:32:37 -0500

    set: UserLogin.js Login

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -1,3 +1,13 @@
 import React, { useState, useEffect } from "react";
 import * as yup from "yup";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+const Login = (props) => {
+  const [userData, setUserData] = useState({
+    username: "",
+    password: "",
+    email: "",
+  });
+};
+
+export default Login;
