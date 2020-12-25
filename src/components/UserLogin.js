4f82c7c75687c0904a5d7b3500abeb48ebbb8655
kwmorlock <kmorlockcodes@gmail.com>
2020-12-25 02:12:15 -0500

    fix: UserLogin.js syntax

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -13,12 +13,27 @@ const Login = (props) => {
     password: yup.string().required("Please enter your password!"),
     email: yup.string().required("Please enter your email!"),
   });
-};
 
-const [errors, setErrors] = useState({
-  username: "",
-  password: "",
-  email: "",
-});
+  const [errors, setErrors] = useState({
+    username: "",
+    password: "",
+    email: "",
+  });
+
+  const formValidation = (e) => {
+    yup
+      .reach(loginFormSchema, e.target.name)
+      .validate(e.target.value)
+      .then((valid) => {
+        setErrors({ ...errors, [e.target.name]: "" });
+      })
+      .catch((err) => {
+        setErrors({
+          ...errors,
+          [e.target.name]: err.errors[0],
+        });
+      });
+  };
+};
 
 export default Login;
