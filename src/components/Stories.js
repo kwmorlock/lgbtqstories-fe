26c7eb4f02d89b8b90418b8d2d168c8abf3bb141
kwmorlock <kmorlockcodes@gmail.com>
2020-12-26 17:23:09 -0500

    set: Stories.js UserStories useState

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,2 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+const UserStories = (props) => {
+  const [stories, setStories] = useState([]);
+  const [searchTag, setSearchTag] = useState("");
+};
+
+export default UserStories;
