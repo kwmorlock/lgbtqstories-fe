acf79adb96efd8ae076cef52b88ce3141ae3cd33
kwmorlock <kmorlockcodes@gmail.com>
2020-12-26 17:40:13 -0500

    set: App.js PrivateRoute stories

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import Header from "./components/Header";
 import Register from "./components/UserReg";
 import Login from "./components/UserLogin";
+import PrivateRoute from "./utils/privateRoute";
+import UserStories from "./components/Stories";
 import { HashRouter as Router, Route } from "react-router-dom";
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
         <p>Welcome</p>
         <Route path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
+        <PrivateRoute path="/stories" component={UserStories} />
       </div>
     </Router>
   );
