52487d838a0052f39bb37baf05f2c0b4272bfa3b
kwmorlock <kmorlockcodes@gmail.com>
2020-12-27 15:07:18 -0500

    set: UserStories.js initialStory

--- src/components/UserStories.js
+++ src/components/UserStories.js
@@ -1,1 +1,10 @@
-//setup
+import React, { useState, useEffect } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+const initialStory = {
+  id: null,
+  title: "",
+  story: "",
+  tags: "",
+  usersId: "",
+};
