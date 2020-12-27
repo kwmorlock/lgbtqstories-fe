150d6eb90159eb7d003ebd2e8852d7efa5bfc629
kwmorlock <kmorlockcodes@gmail.com>
2020-12-27 01:17:37 -0500

    set: PostStories.js initialState

--- src/components/PostStories.js
+++ src/components/PostStories.js
@@ -2,3 +2,10 @@ import React, { useState } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const storyId = window.localStorage.getItem("id");
+
+const initialState = {
+    title: "",
+    story: "",
+    tags: "",
+    usersId: storyId
+  };
