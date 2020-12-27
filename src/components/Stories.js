715668a671c9774329fa8dfb158c87bffe4be833
kwmorlock <kmorlockcodes@gmail.com>
2020-12-26 20:07:41 -0500

    set: Stories.js styling

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -47,7 +47,7 @@ const UserStories = (props) => {
 
         <div>
           {stories.map((stories) => (
-            <div key={stories.id}>
+            <div key={stories.id} style={{ margin: "20px auto", width: "30%", backgroundColor: "hotpink", border: "3px solid purple" }}>
               <div key={stories.id}>
                 <p>Title: {stories.title}</p>
                 <p>Story: {stories.story}</p>
