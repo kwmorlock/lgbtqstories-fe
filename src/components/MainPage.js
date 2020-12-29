ec7629911429a480885deb763eeef1f90bbcbbef
kwmorlock <kmorlockcodes@gmail.com>
2020-12-29 01:41:43 -0500

    set: MainPage.js welcome

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,1 +1,13 @@
-//setup
+import React from "react";
+
+const MainPage = () => {
+  return (
+    <>
+      <div>
+        <h1>Welcome to LGBTQ Stories where you can share anonymous stories about your own experiences, keep track of your own stories, and even edit or delete them at a later time.</h1>
+      </div>
+    </>
+  );
+};
+
+export default MainPage;
