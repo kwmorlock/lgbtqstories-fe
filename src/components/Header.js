a9acaaba95a4f379d9b88504132bbd66c2c381a9
kwmorlock <kmorlockcodes@gmail.com>
2020-12-29 18:54:53 -0500

    set: Header.js Links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
       <div>
         <h1>LGBTQ Stories</h1>
+        <div
+          class="color"
+          style={{
+            margin: "20px auto",
+            width: "10%",
+            backgroundColor: "hotpink",
+            border: "3px solid purple",
+          }}
+        >
+          <Link to="/">Home</Link>
+        </div>
+        <div
+          class="color"
+          style={{
+            margin: "20px auto",
+            width: "10%",
+            backgroundColor: "hotpink",
+            border: "3px solid purple",
+          }}
+        >
+          <Link to="/login">Login</Link>
+        </div>
+        <div
+          class="color"
+          style={{
+            margin: "20px auto",
+            width: "10%",
+            backgroundColor: "hotpink",
+            border: "3px solid purple",
+          }}
+        >
+          <Link to="/register">Register</Link>
+        </div>
       </div>
     </>
   );
