00fe8f8d064009d4af38848fb9d47f720331a536
kwmorlock <kmorlockcodes@gmail.com>
2020-12-29 01:43:13 -0500

    cle: MainPage.js format

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -4,7 +4,11 @@ const MainPage = () => {
   return (
     <>
       <div>
-        <h1>Welcome to LGBTQ Stories where you can share anonymous stories about your own experiences, keep track of your own stories, and even edit or delete them at a later time.</h1>
+        <h1>
+          Welcome to LGBTQ Stories where you can share anonymous stories about
+          your own experiences, keep track of your own stories, and even edit or
+          delete them at a later time.
+        </h1>
       </div>
     </>
   );
