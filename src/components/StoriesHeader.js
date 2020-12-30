f78ebcc5a4c7161aaeeff64e4b3283b2995a14b5
kwmorlock <kmorlockcodes@gmail.com>
2020-12-29 20:18:32 -0500

    set: StoriesHeader.js links

--- src/components/StoriesHeader.js
+++ src/components/StoriesHeader.js
@@ -1,1 +1,112 @@
-//setup
+import React from "react";
+import { Link } from "react-router-dom";
+
+const StoriesHeader = () => {
+  return (
+    <>
+      <div class="header">
+        <h1>LGBTQ Stories</h1>
+        <div
+          style={{
+            margin: "20px auto",
+            width: "10%",
+            fontSize: "2rem",
+          }}
+        >
+          <Link
+            style={{
+              color: "white",
+              textDecoration: "none",
+            }}
+            to="/"
+          >
+            Home
+          </Link>
+        </div>
+        <div
+          style={{
+            margin: "20px auto",
+            width: "10%",
+            fontSize: "2rem",
+          }}
+        >
+          <Link
+            style={{
+              color: "white",
+              textDecoration: "none",
+            }}
+            to="/login"
+          >
+            Login
+          </Link>
+        </div>
+        <div
+          style={{
+            color: "white",
+            margin: "20px auto",
+            width: "10%",
+            fontSize: "2rem",
+          }}
+        >
+          <Link
+            style={{
+              color: "white",
+              textDecoration: "none",
+            }}
+            to="/register"
+          >
+            Register
+          </Link>
+        </div>
+        <div
+          style={{
+            color: "white",
+            margin: "20px auto",
+            width: "10%",
+            fontSize: "2rem",
+            background: "none",
+          }}
+        >
+          <a href="https://github.com/kwmorlock/lgbtqstories-fe">
+            <button
+              style={{
+                color: "white",
+                textDecoration: "none",
+                background: "none",
+                border: "none",
+                fontSize: "2rem",
+              }}
+            >
+              FE Github
+            </button>
+          </a>
+        </div>
+        <div
+          style={{
+            color: "white",
+            margin: "20px auto",
+            width: "10%",
+            fontSize: "2rem",
+            background: "none",
+          }}
+        >
+          <a href="https://github.com/kwmorlock/lgbtqstories-be">
+            <button
+              style={{
+                color: "white",
+                textDecoration: "none",
+                background: "none",
+                border: "none",
+                fontSize: "2rem",
+              }}
+            >
+              BE Github
+            </button>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default StoriesHeader;
