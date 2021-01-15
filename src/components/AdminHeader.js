d566796bf1a5e171343279cc6b158a1584962016
kwmorlock <kmorlockcodes@gmail.com>
2021-01-15 03:00:52 -0500

    set: AdminHeader.js AdminHeader

--- src/components/AdminHeader.js
+++ src/components/AdminHeader.js
@@ -1,1 +1,138 @@
-//setup
+import React from "react";
+import { Link } from "react-router-dom";
+
+const AdminHeader = () => {
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = "/#/adminlogin";
+  };
+  return (
+    <>
+      <div class="header">
+        <h1
+          style={{
+            marginLeft: "1%",
+          }}
+        >
+          LGBTQ Stories
+        </h1>
+        <div
+          style={{
+            margin: "20px auto",
+            fontSize: "2rem",
+          }}
+        >
+          <Link
+            style={{
+              color: "white",
+              textDecoration: "none",
+            }}
+            to="/stories"
+          >
+            All Stories
+          </Link>
+        </div>
+
+        <div
+          style={{
+            margin: "20px auto",
+            fontSize: "2rem",
+          }}
+        >
+          <Link
+            style={{
+              color: "white",
+              textDecoration: "none",
+            }}
+            to="/stories/edit"
+          >
+            Your Stories
+          </Link>
+        </div>
+        <div
+          style={{
+            margin: "20px auto",
+            fontSize: "2rem",
+          }}
+        >
+          <Link
+            style={{
+              color: "white",
+              textDecoration: "none",
+            }}
+            to="/stories/add"
+          >
+            Add Story
+          </Link>
+        </div>
+        <div
+          style={{
+            color: "white",
+            margin: "20px auto",
+            fontSize: "2rem",
+            background: "none",
+          }}
+        >
+          <a href="https://github.com/kwmorlock/lgbtqstories-fe">
+            <button
+              style={{
+                color: "white",
+                textDecoration: "none",
+                background: "none",
+                border: "none",
+                fontSize: "2rem",
+              }}
+            >
+              FE Github
+            </button>
+          </a>
+        </div>
+        <div
+          style={{
+            color: "white",
+            margin: "20px auto",
+            fontSize: "2rem",
+            background: "none",
+          }}
+        >
+          <a href="https://github.com/kwmorlock/lgbtqstories-be">
+            <button
+              style={{
+                color: "white",
+                textDecoration: "none",
+                background: "none",
+                border: "none",
+                fontSize: "2rem",
+              }}
+            >
+              BE Github
+            </button>
+          </a>
+        </div>
+        <div
+          style={{
+            color: "white",
+            margin: "20px auto",
+            fontSize: "2rem",
+            background: "none",
+          }}
+        >
+          <button
+            style={{
+              color: "white",
+              textDecoration: "none",
+              background: "none",
+              border: "none",
+              fontSize: "2rem",
+            }}
+            onClick={() => logout(false)}
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AdminHeader;
