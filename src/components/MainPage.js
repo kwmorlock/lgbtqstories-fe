1e29d8ffa2149fa383f9d8442341b012fb755c34
kwmorlock <kmorlockcodes@gmail.com>
2021-01-21 11:00:33 -0500

    sty: MainPageStyles.js MainDiv

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Header from "./Header";
+import { MainDiv } from "./MainPageStyles";
 
 const MainPage = () => {
   return (
     <>
       <Header />
-      <div class="main">
+      <MainDiv>
         <h1 class="whitetext">
           Welcome to LGBTQ Stories where you can share anonymous stories about
           your own experiences, keep track of your own stories, and even edit or
           delete them at a later time.
         </h1>
-      </div>
+      </MainDiv>
     </>
   );
 };
--- src/components/MainPageStyles.js
+++ src/components/MainPageStyles.js
@@ -1,1 +1,7 @@
-//setup
+import styled from "styled-components";
+
+export const MainDiv = styled.div`
+  width: 50%;
+  margin: auto;
+  font-size: 2rem;
+`;
